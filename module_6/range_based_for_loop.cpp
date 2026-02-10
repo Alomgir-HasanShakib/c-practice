a7af68172be9b6f3723ca9c5a4760449208908ca
AH Shakib <alomgirhasanshakib@gmail.com>
2026-02-10 22:15:41 +0600

    practice string and range based loop reverse function and solve a problem using stringstream and reverse function concept

---
+++ module_6/range_based_for_loop.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    getline(cin, s);
+
+    // shortcut  for loop  to pring single character from an string
+    for (char c : s)
+        cout << c << endl;
+
+    cout << s << endl;
+    return 0;
+}
